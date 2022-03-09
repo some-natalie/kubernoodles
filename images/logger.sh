df39ccdccd1dabe9dc214f79bb0cc1b18f8814ae
Natalie Somersall <some-natalie@github.com>
2022-03-09 15:29:10 -0700

    move the logger to bash + clean up

--- images/logger.sh
+++ images/logger.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 # Logger from this post http://www.cubicrace.com/2016/03/log-tracing-mechnism-for-shell-scripts.html
 
-INFO(){
-    _info_function_name="${FUNCNAME[1]}"
-    _info_msg="$1"
+function INFO(){
+    local function_name="${FUNCNAME[1]}"
+    local msg="$1"
     timeAndDate=$(date)
-    echo "[$timeAndDate] [INFO] [${0}] $_info_msg"
+    echo "[$timeAndDate] [INFO] [${0}] $msg"
 }
 
 
-DEBUG(){
-    _debug_function_name="${FUNCNAME[1]}"
-    _debug_msg="$1"
+function DEBUG(){
+    local function_name="${FUNCNAME[1]}"
+    local msg="$1"
     timeAndDate=$(date)
-    echo "[$timeAndDate] [DEBUG] [${0}] $_debug_msg"
+    echo "[$timeAndDate] [DEBUG] [${0}] $msg"
 }
 
-ERROR(){
-    _error_function_name="${FUNCNAME[1]}"
-    _error_msg="$1"
+function ERROR(){
+    local function_name="${FUNCNAME[1]}"
+    local msg="$1"
     timeAndDate=$(date)
-    echo "[$timeAndDate] [ERROR]  $_error_msg"
+    echo "[$timeAndDate] [ERROR] [${0}] $msg"
 }
