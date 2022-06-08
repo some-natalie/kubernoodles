eeab7fa1a8edcaaae9fb0772f59735111044224c
Natalie Somersall <some-natalie@github.com>
2022-06-08 15:46:11 -0600

    update rootless-startup script

--- images/rootless-startup.sh
+++ images/rootless-startup.sh
@@ -2,7 +2,7 @@
 source /opt/bash-utils/logger.sh
 
 INFO "Starting Docker (rootless)"
-/home/runner/bin/dockerd-rootless.sh -H tcp://0.0.0.0:2376 >> /dev/null 2>&1 &
+/home/runner/bin/dockerd-rootless.sh --config-file /home/runner/.config/docker/daemon.json >> /dev/null 2>&1 &
 
 # Wait processes to be running
 entrypoint.sh
