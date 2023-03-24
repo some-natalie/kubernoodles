d9300fd3da087e6d9a78f9a2c7f92796dabfa485
Natalie Somersall <some-natalie@github.com>
2023-03-24 14:49:47 -0600

    Remove old arc (#181)
    * remove lots of flies for images
    
    * rewrite release
    
    * remove more files
    
    * add gitignore
    
    * update readme
    
    * remove pages
    
    * remove old deployment files
    
    * remove ghae

--- images/podman-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /usr/bin/logger.sh
-
-# Symlink the cache assets
-log.notice "Symlinking static cache assets"
-ln -s /opt/statictoolcache/* /opt/hostedtoolcache && ls -l /opt/hostedtoolcache
-
-# Wait processes to be running
-entrypoint.sh
