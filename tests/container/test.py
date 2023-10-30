fa2d454f7fd9abafceb9f8519a515513ad5ccc43
Natalie Somersall <some-natalie@github.com>
2023-10-30 11:28:44 -0600

    swap table for bullet points

--- tests/container/test.py
+++ tests/container/test.py
@@ -4,4 +4,4 @@ import os
 
 print("It looks like container Actions work exactly as expected.")
 print("Have a nice day!")
-os.subprocess("echo '| ✅ | container action succeeded |' >> $GITHUB_STEP_SUMMARY")
+os.subprocess("echo '- ✅ container action succeeded' >> $GITHUB_STEP_SUMMARY")
