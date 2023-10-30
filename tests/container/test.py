82b148646b1fafa9449e72f6763980f6d08401e5
Natalie Somersall <some-natalie@github.com>
2023-10-30 11:13:44 -0600

    attempt step outputs on tests

--- tests/container/test.py
+++ tests/container/test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 print("It looks like container Actions work exactly as expected.")
 print("Have a nice day!")
+os.subprocess("echo '| ✅ | container action succeeded |' >> $GITHUB_STEP_SUMMARY")
